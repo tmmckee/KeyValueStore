e93aaca5023f19e7b6151e19c11801f336b8c569
Tyler Mckee <tmmckee97@gmail.com>
2019-09-11 08:15:26 -0400

    Add project files.

---
+++ KeyValueStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KeyValueStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
